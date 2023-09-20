a0421e61a7b19773e0cb86b45a18c242cdbe81d2
Fizza Jafri <jafrifm@gmail.com>
2023-09-20 16:34:38 -0400

    [refactor]: implement error handling for playerInitials command line argument

--- play.js
+++ play.js
@@ -5,10 +5,25 @@ const { setupInput } = require("./input");
 // receive playerInitials as a command line argument
 const playerInitials = process.argv.slice(2);
 
-console.log("Connecting ...");
+// validate playerInitials before running game
+const validateInput = function(playerInitials) {
+  // if the string is more than 3 characters long || if more than 1 argument is provided || if the string contains any characters other than letters
+  if (playerInitials[0].length > 3 || playerInitials.length > 1 || !(/^[A-Za-z]+$/.test(playerInitials[0]))) {
+    console.error("Error: invalid initials");
+    process.exit();
+  }
 
-// pass the playerInitials into the connect function
-const conn = connect(playerInitials);
+  console.log("Connecting ...");
 
-// pass the conn object into the setupInput function
-setupInput(conn);
+  // pass the playerInitials into the connect function
+  const conn = connect(playerInitials);
+
+  // pass the conn object into the setupInput function
+  setupInput(conn);
+};
+
+validateInput(playerInitials);
+
+
+
+
